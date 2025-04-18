9b828abfdd1d51ff57c9a6d06513f49952676045
Pranay Bhoir <70322184+pranayb1256@users.noreply.github.com>
2025-04-18 22:51:22 +0530

    added on render

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -39,8 +39,16 @@ app.use(
 import path from "path";
 import { fileURLToPath } from "url";
 
+// This is necessary when using ES modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+// Serve static frontend files from dist
+app.use(express.static(path.join(__dirname, "../../Frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../Frontend/dist", "index.html"));
+});
 
 // Serve static files from frontend/dist
 app.use(express.static(path.join(__dirname, "../Frontend/dist")));
