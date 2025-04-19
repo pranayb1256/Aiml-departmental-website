af6cfef32f38b30573d28346dd6b1b5e2dd498fd
Pranay Bhoir <70322184+pranayb1256@users.noreply.github.com>
2025-04-19 10:41:32 +0530

    added on render

--- Frontend/src/Components/ImageCarousel/ImageCarousel.jsx
+++ Frontend/src/Components/ImageCarousel/ImageCarousel.jsx
@@ -15,7 +15,7 @@ const ImageCarousel = () => {
 
   useEffect(() => {
     let isMounted = true; // Prevent setting state on unmounted component
-    const apiUrl = ;
+    const apiUrl = import.meta.env.VITE_API_URL;
 
     const fetchEventImages = async () => {
       try {
